8261958d66f3d097f8a22dadf68733114fefd1e9
AwdreyAnjos <awdrey.Anjos@gmail.com>
2022-10-05 01:38:03 +0100

    Finalizando atividade adivinhação

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,52 +1,63 @@
+import os # importe lib para limpar o terminal do python
 import random #importe para números aleátorios 
 
+os.system('cls' if os.name == 'nt' else 'clear'); # para limpar o terminal
+# cls para windows 
+# clear para linux
 
-print("------------------------------------");
-print("| Bem vindo ao jogo de adivinhação |");
-print("------------------------------------");
+print("              ------------------------------------");
+print("              | BEM VINDO AO JOGO DE ADIVINHAÇÃO |");
+print("              ------------------------------------\n");
 
-print("------------------ Escolha a dificuldade ------------------")
+print("------------------ ESCOLHA SUA DIFICULDADE ------------------\n")
 f = 10;
 m = 20;
 d = 50;
-print("            f (FACIL) | m (MEDIO) | d (DIFICIL)")
-escolha = input("------> ")
+print(" ---------> f (FACIL) | m (MEDIO) | d (DIFICIL) <------------\n");
 
-if escolha == "f":
-    numero_secreto = random.randint(1,f)
-elif escolha == "m":
-    numero_secreto = random.randint(1,m)
-elif escolha == "d":
-    numero_secreto = random.randint(1,d)
-else:
-    print("DIFICULDADE NÃO ENCONTRADA")
 
-print(numero_secreto)
+for x in range(3): # Escolhendo dificuldade do sorteio
+    escolha = input("----------> ");
+    x = escolha;
+    if escolha == "f": # nivel facil - random  1 ao 10
+        numero_secreto = random.randint(1,f);
+        break
+    elif escolha == "m": # nivel medio - random 1 ao 20
+        numero_secreto = random.randint(1,m);
+        break
+    elif escolha == "d": # nivel dificil - random 1 ao 50
+        numero_secreto = random.randint(1,d);
+        break
+    else:
+        print("\n------------- DIFICULDADE NÃO ENCONTRADA ---------------");
+        print("------------------ TENTE NOVAMENTE -----------------\n")
+
 
 for i in range(3): # for que vai rodar até o número de tentativas acabar
+
     numero_usuario = int(input("\nDigite seu número da sorte --> "));
 
     if numero_usuario == numero_secreto: #caso o usuário acerte
-        print("\n***********************");
-        print("PARABÉNS VOCÊ ACERTOU!!");
-        print("***********************");
+        print("\n                ***********************");
+        print("                PARABÉNS VOCÊ ACERTOU!!");
+        print("                ***********************");
         break
 
     elif i < 2 and numero_secreto < numero_usuario: #caso não esteja certo e seja maior que o sorteado
-        print("\n====================================")
-        print("Errado, tem mais",i+1,"de 3 Tentativas")
-        print("\nTente um número mais baixo")
-        print("====================================")
+        print("\n        ======================================");
+        print("          ERRADO, TEM MAIS",i+1,"DE 3 TENTATIVAS");
+        print("\n             TENTE UM NÚMERO MAIS BAIXO");
+        print("        ======================================");
 
     elif i < 2 and numero_secreto > numero_usuario: #caso não esteja certo e seja menor que o sorteado
-        print("\n====================================")
-        print("Errado, tem mais",i+1,"de 3 Tentativas")
-        print("\nTente um número mais alto")
-        print("====================================")
+        print("\n        ======================================");
+        print("          ERRADO, TEM MAIS",i+1,"DE 3 TENTATIVAS");
+        print("\n               TENTE UM NÚMERO MAIS ALTO");
+        print("        ======================================");
 
     elif i >= 2: #caso o usuário erre todas as tentativas 
-        print("»»»»»»»»»»»»»»»»»»»")
-        print("Não foi dessa vez!!")
-        print("»»»»»»»»»»»»»»»»»»»")
-        print("O NÚMERO SORTEADO ERA ------> ", numero_secreto )
+        print("                »»»»»»»»»»»»»»»»»»»");
+        print("                 NÃO FOI DESSA VEZ!");
+        print("                »»»»»»»»»»»»»»»»»»»");
+        print("           O NÚMERO SORTEADO ERA ------> ", numero_secreto );
 
