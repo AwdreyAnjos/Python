3a5a99de1766e4cccabb808781b21b08b5eff1d3
AwdreyAnjos <awdrey.Anjos@gmail.com>
2022-10-05 03:50:42 +0100

    Melhoria - for pelo while

--- adivinhacao.py
+++ adivinhacao.py
@@ -15,49 +15,53 @@ m = 20;
 d = 50;
 print(" ---------> f (FACIL) | m (MEDIO) | d (DIFICIL) <------------\n");
 
+x = False
 
-for x in range(3): # Escolhendo dificuldade do sorteio
+while (x == False): # Escolhendo dificuldade do sorteio
     escolha = input("----------> ");
     x = escolha;
-    if escolha == "f": # nivel facil - random  1 ao 10
+    if (escolha == "f"): # nivel facil - random  1 ao 10
         numero_secreto = random.randint(1,f);
-        break
-    elif escolha == "m": # nivel medio - random 1 ao 20
+        x = True
+    elif (escolha == "m"): # nivel medio - random 1 ao 20
         numero_secreto = random.randint(1,m);
-        break
-    elif escolha == "d": # nivel dificil - random 1 ao 50
+        x = True
+    elif (escolha == "d"): # nivel dificil - random 1 ao 50
         numero_secreto = random.randint(1,d);
-        break
+        x = True
     else:
         print("\n------------- DIFICULDADE NÃO ENCONTRADA ---------------");
         print("------------------ TENTE NOVAMENTE -----------------\n")
+        x = False
 
 
 for i in range(3): # for que vai rodar até o número de tentativas acabar
 
     numero_usuario = int(input("\nDigite seu número da sorte --> "));
 
-    if numero_usuario == numero_secreto: #caso o usuário acerte
+    if i >= 2: #caso o usuário erre todas as tentativas 
+        print("\n                »»»»»»»»»»»»»»»»»»»");
+        print("                 NÃO FOI DESSA VEZ!");
+        print("                »»»»»»»»»»»»»»»»»»»");
+        print("           O NÚMERO SORTEADO ERA ------> {}\n".format( numero_secreto));
+
+    elif (numero_usuario == numero_secreto): #caso o usuário acerte
         print("\n                ***********************");
         print("                PARABÉNS VOCÊ ACERTOU!!");
         print("                ***********************");
         break
 
-    elif i < 2 and numero_secreto < numero_usuario: #caso não esteja certo e seja maior que o sorteado
+    elif (numero_secreto < numero_usuario): #caso não esteja certo e seja maior que o sorteado
         print("\n        ======================================");
-        print("          ERRADO, TEM MAIS",i+1,"DE 3 TENTATIVAS");
+        print("          ERRADO, TEM MAIS {} DE 3 TENTATIVAS".format(i+1));
         print("\n             TENTE UM NÚMERO MAIS BAIXO");
         print("        ======================================");
 
-    elif i < 2 and numero_secreto > numero_usuario: #caso não esteja certo e seja menor que o sorteado
+    elif (numero_secreto > numero_usuario): #caso não esteja certo e seja menor que o sorteado
         print("\n        ======================================");
-        print("          ERRADO, TEM MAIS",i+1,"DE 3 TENTATIVAS");
+        print("          ERRADO, TEM MAIS {} DE 3 TENTATIVAS".format(i+1));
         print("\n               TENTE UM NÚMERO MAIS ALTO");
         print("        ======================================");
 
-    elif i >= 2: #caso o usuário erre todas as tentativas 
-        print("                »»»»»»»»»»»»»»»»»»»");
-        print("                 NÃO FOI DESSA VEZ!");
-        print("                »»»»»»»»»»»»»»»»»»»");
-        print("           O NÚMERO SORTEADO ERA ------> ", numero_secreto );
+
 
